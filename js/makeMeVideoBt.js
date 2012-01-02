dd193799d769c986448b7d156245aacddc9d75cf
Romain Avalle <romain.avalle@gmail.com>
2012-01-02 17:03:03 +0100

    first commit

---
+++ js/makeMeVideoBt.js
@@ -0,0 +1,34 @@
+jQuery.fn.makeMeVideoBt = function(options){
+	var defaults = {
+            img:'img/play.png'
+    },
+    $el = $(this),
+    options = $.extend(defaults, options);
+    var bt_icon = new Image();
+   	bt_icon.onload = function (){options.width = this.width,options.height = this.height,setIcon()};
+    bt_icon.src = options.img;
+
+    function setIcon(){
+		$el.each(function(){	
+			var t = $(this),
+			w = t.width(),
+			h = t.height(),
+			div = $('<a/>', {
+				href:'#'
+			}),
+			top = t.position().top + parseInt(t.css('margin-top'),10) + parseInt(t.css('padding-top'),10),
+			left = t.position().left + parseInt(t.css('margin-left'),10) + parseInt(t.css('padding-left'),10),
+		
+			css_obj = {
+				'position':'absolute',
+				'top':top + h*.5 - defaults.height * .5+'px',
+				'left':left + w*.5 - defaults.width * .5+'px',
+				'background':'url('+options.img+') no-repeat',
+				'width':options.width+'px',
+				'height':options.height+'px'
+			}
+			div.css(css_obj);
+			t.after(div)
+		})
+	}
+}
