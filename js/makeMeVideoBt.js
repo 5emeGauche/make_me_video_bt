0ef98a3a60975876d50389c088775dd4ff79e177
Romain Avalle <romain.avalle@gmail.com>
2012-01-03 10:15:37 +0100

    update htmlTag + position

--- js/makeMeVideoBt.js
+++ js/makeMeVideoBt.js
@@ -1,6 +1,8 @@
 jQuery.fn.makeMeVideoBt = function(options){
 	var defaults = {
-            img:'img/play.png'
+            img:'img/play.png',
+            position:'coverAll',
+            htmlTag:'link'
     },
     $el = jQuery(this),
     options = jQuery.extend(defaults, options);
@@ -18,14 +20,29 @@ jQuery.fn.makeMeVideoBt = function(options){
 			}),
 			top = t.position().top + parseInt(t.css('margin-top'),10) + parseInt(t.css('padding-top'),10),
 			left = t.position().left + parseInt(t.css('margin-left'),10) + parseInt(t.css('padding-left'),10),
-		
-			css_obj = {
-				'position':'absolute',
-				'top':top + h*.5 - defaults.height * .5+'px',
-				'left':left + w*.5 - defaults.width * .5+'px',
-				'background':'url('+options.img+') no-repeat',
-				'width':options.width+'px',
-				'height':options.height+'px'
+			css_obj = {}
+			if(options.htmlTag == 'span'){
+				div = $('<span/>')
+			}
+			if(options.position == 'clip' ){				
+				css_obj = {
+					'position':'absolute',
+					'top':top + h*.5 - defaults.height * .5+'px',
+					'left':left + w*.5 - defaults.width * .5+'px',
+					'background':'url('+options.img+') no-repeat',
+					'width':options.width+'px',
+					'height':options.height+'px'
+				}
+			}
+			if(options.position == 'coverAll' ){				
+				css_obj = {
+					'position':'absolute',
+					'top':top +'px',
+					'left':left +'px',
+					'background':'url('+options.img+') no-repeat 50% 50%',
+					'width':w+'px',
+					'height':h+'px'
+				}
 			}
 			div.css(css_obj);
 			t.after(div)
